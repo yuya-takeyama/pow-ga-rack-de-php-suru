2fb761b81127212729bc60a8384adc188d8f588c
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-05-24 03:19:16 +0900

    Fix.

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../silex.phar';
 $app = new Silex\Application();
 
 $app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name) . '. This is Silex app as Rack running on Pow!';
+    return "Hello {$app->escape($name)}. This is Silex app as Rack running on Pow!";
 });
 
 $app->get('/', function () use ($app) {
