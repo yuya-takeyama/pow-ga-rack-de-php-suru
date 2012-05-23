7bd10dcc810ecb5a9e7e22533371e95c1223c194
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-05-24 02:58:24 +0900

    Fix example Silex app.

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,8 +3,12 @@ require_once __DIR__ . '/../silex.phar';
 
 $app = new Silex\Application();
 
-$app->get('/hello/{name}', function($name) use($app) {
-    return 'Hello '.$app->escape($name) . '. This is Silex app running on Rack!';
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello '.$app->escape($name) . '. This is Silex app as Rack running on Pow!';
+});
+
+$app->get('/', function () use ($app) {
+    return 'Hello, World!';
 });
 
 $app->run();
